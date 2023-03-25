96ca46aa84a179c4d8f17dffcf2edfc04c63b346
GuiltZetus <davikzetus@gmail.com>
2023-03-25 20:32:32 +0700

    version 1.5

--- routes/authors.js
+++ routes/authors.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const router = express.Router()
-
+const Author = require('../models/author')
 // All Authors route
 router.get('/', (req,res)=> {
     res.render('authors/index')
@@ -8,12 +8,28 @@ router.get('/', (req,res)=> {
 
 // New authors route
 router.get('/new', (req,res) => {
-    res.render('authors/new')
+    res.render('authors/new', { author : new Author() })
 })
 
 // Create Author Route
-router.post('/',(req,res) => {
-    res.send('Create')
+router.post('/', async (req,res) => {
+    const author = new Author({
+        name : req.body.name
+    }) 
+    try {
+        const newAuthor = await author.save()
+        res.redirect('authors')
+    }
+    catch {
+        res.render('authors/new', {
+        author : author,
+        errorMessage : 'error creating author'
+        })
+    } 
 })
 
+
+  
 module.exports = router
+
+
